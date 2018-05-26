e38f45fba40e21e0cac080f248ef0cd1a63b935d
Roberto Villarejo Martínez <robertovillarejo@outlook.com>
2018-05-25 22:29:52 -0500

    Video entity

---
+++ src/main/java/io/github/robertovillarejo/domain/Video.java
@@ -0,0 +1,108 @@
+package io.github.robertovillarejo.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A Video.
+ */
+@Document(collection = "video")
+public class Video implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id;
+
+    @Field("title")
+    private String title;
+
+    @Field("description")
+    private String description;
+
+    @Field("url")
+    private String url;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Video title(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Video description(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Video url(String url) {
+        this.url = url;
+        return this;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Video video = (Video) o;
+        if (video.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), video.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Video{" +
+            "id=" + getId() +
+            ", title='" + getTitle() + "'" +
+            ", description='" + getDescription() + "'" +
+            ", url='" + getUrl() + "'" +
+            "}";
+    }
+}
---
+++ src/main/java/io/github/robertovillarejo/web/rest/VideoResource.java
@@ -0,0 +1,118 @@
+package io.github.robertovillarejo.web.rest;
+
+import com.codahale.metrics.annotation.Timed;
+import io.github.robertovillarejo.domain.Video;
+import io.github.robertovillarejo.service.VideoService;
+import io.github.robertovillarejo.web.rest.errors.BadRequestAlertException;
+import io.github.robertovillarejo.web.rest.util.HeaderUtil;
+import io.github.jhipster.web.util.ResponseUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * REST controller for managing Video.
+ */
+@RestController
+@RequestMapping("/api")
+public class VideoResource {
+
+    private final Logger log = LoggerFactory.getLogger(VideoResource.class);
+
+    private static final String ENTITY_NAME = "video";
+
+    private final VideoService videoService;
+
+    public VideoResource(VideoService videoService) {
+        this.videoService = videoService;
+    }
+
+    /**
+     * POST  /videos : Create a new video.
+     *
+     * @param video the video to create
+     * @return the ResponseEntity with status 201 (Created) and with body the new video, or with status 400 (Bad Request) if the video has already an ID
+     * @throws URISyntaxException if the Location URI syntax is incorrect
+     */
+    @PostMapping("/videos")
+    @Timed
+    public ResponseEntity<Video> createVideo(@RequestBody Video video) throws URISyntaxException {
+        log.debug("REST request to save Video : {}", video);
+        if (video.getId() != null) {
+            throw new BadRequestAlertException("A new video cannot already have an ID", ENTITY_NAME, "idexists");
+        }
+        Video result = videoService.save(video);
+        return ResponseEntity.created(new URI("/api/videos/" + result.getId()))
+            .headers(HeaderUtil.createEntityCreationAlert(ENTITY_NAME, result.getId().toString()))
+            .body(result);
+    }
+
+    /**
+     * PUT  /videos : Updates an existing video.
+     *
+     * @param video the video to update
+     * @return the ResponseEntity with status 200 (OK) and with body the updated video,
+     * or with status 400 (Bad Request) if the video is not valid,
+     * or with status 500 (Internal Server Error) if the video couldn't be updated
+     * @throws URISyntaxException if the Location URI syntax is incorrect
+     */
+    @PutMapping("/videos")
+    @Timed
+    public ResponseEntity<Video> updateVideo(@RequestBody Video video) throws URISyntaxException {
+        log.debug("REST request to update Video : {}", video);
+        if (video.getId() == null) {
+            return createVideo(video);
+        }
+        Video result = videoService.save(video);
+        return ResponseEntity.ok()
+            .headers(HeaderUtil.createEntityUpdateAlert(ENTITY_NAME, video.getId().toString()))
+            .body(result);
+    }
+
+    /**
+     * GET  /videos : get all the videos.
+     *
+     * @return the ResponseEntity with status 200 (OK) and the list of videos in body
+     */
+    @GetMapping("/videos")
+    @Timed
+    public List<Video> getAllVideos() {
+        log.debug("REST request to get all Videos");
+        return videoService.findAll();
+        }
+
+    /**
+     * GET  /videos/:id : get the "id" video.
+     *
+     * @param id the id of the video to retrieve
+     * @return the ResponseEntity with status 200 (OK) and with body the video, or with status 404 (Not Found)
+     */
+    @GetMapping("/videos/{id}")
+    @Timed
+    public ResponseEntity<Video> getVideo(@PathVariable String id) {
+        log.debug("REST request to get Video : {}", id);
+        Video video = videoService.findOne(id);
+        return ResponseUtil.wrapOrNotFound(Optional.ofNullable(video));
+    }
+
+    /**
+     * DELETE  /videos/:id : delete the "id" video.
+     *
+     * @param id the id of the video to delete
+     * @return the ResponseEntity with status 200 (OK)
+     */
+    @DeleteMapping("/videos/{id}")
+    @Timed
+    public ResponseEntity<Void> deleteVideo(@PathVariable String id) {
+        log.debug("REST request to delete Video : {}", id);
+        videoService.delete(id);
+        return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert(ENTITY_NAME, id)).build();
+    }
+}
